fd98e5b1a472a2d35853d464bb1ba2bfcdd30bd4
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-08-19 13:19:17 -0500

    switch back between groups depending on MFA status.

--- bin/check_accounts.rb
+++ bin/check_accounts.rb
@@ -2,11 +2,11 @@ require 'aws-sdk-core'
 require 'trollop'
 
 opts = Trollop::options do
-  opt :read_only_group, "name of the read only group to assign non-mfa'd users to.", required: true, type: String
+  opt :read_only_group, "name of the read only group to assign non-mfa'd users to. Any users that have MFAs will be moved to the full-privileged group.", required: true, type: String
   opt :full_privilege_group, "name of the full full privileged group group to assign mfa'd users to. The script will only inspect users of this group.", required: true, type: String
 end
 
-def privileged_user? iam, privileged_group, username
+def member_of_group? iam, privileged_group, username
   result = false
   user_groups = iam.list_groups_for_user(user_name: username).groups
   user_groups.each do |group| 
@@ -24,11 +24,17 @@ iam = Aws::IAM::Client.new(region: 'us-east-1')
 
 all_users = iam.list_users.users
 all_users.each do |user|
-  if privileged_user? iam, opts[:full_privilege_group], user.user_name
+  if member_of_group? iam, opts[:full_privilege_group], user.user_name
     if iam.list_mfa_devices(user_name: user.user_name).mfa_devices.size == 0
       puts "privileged user #{user.user_name} does not have an MFA attached, moving to read only group #{opts[:read_only_group]}"
       iam.add_user_to_group user_name: user.user_name, group_name: opts[:read_only_group]
       iam.remove_user_from_group user_name: user.user_name, group_name: opts[:full_privilege_group]
+    end
+  elsif member_of_group? iam, opts[:read_only_group], user.user_name
+    if iam.list_mfa_devices(user_name: user.user_name).mfa_devices.size > 0
+      puts "privileged user #{user.user_name} has an MFA attached, moving to full-privileged group #{opts[:full_privilege_group]}"
+      iam.add_user_to_group user_name: user.user_name, group_name: opts[:full_privilege_group]
+      iam.remove_user_from_group user_name: user.user_name, group_name: opts[:read_only_group]
     end
   end
 end
